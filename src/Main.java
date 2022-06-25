5b11724dfd9f02632c1f99fb07f8730f7d8a929c
nathalia carlucci <nathalia.carlucci.7@gmail.com>
2022-06-25 13:05:42 -0300

    Digite três números e aparecerá o maior

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+	public class Main {
+		public static void main(String[] args) {
+			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("Digite três números:");
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			
+			if (a > b && a > c) {
+				System.out.println("O maior = " + a);
+			} else if (b > c) {
+				System.out.println("O maior = " + b);
+			} else {
+				System.out.println("O maior = " + c);
+			}
+			sc.close();
+	}
+}
+
